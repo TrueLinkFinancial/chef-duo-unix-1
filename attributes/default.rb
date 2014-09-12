68377c0ea868ad41b643d4065ce3f0318c225510
James Burns <jfburns@gmail.com>
2014-09-11 17:25:47 -0700

    adding rest of current duo config file options

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,12 +3,20 @@ default['duo_unix']['version']  = '1.9'
 default['duo_unix']['checksum'] = '8b602272bdd8f23d050fb6e8e60b4b23a48428c001d5bc2bea1ac56e9c2c2d6a74d41bc1ba26209c12d56238a9048b53ab6a6f2b5b854a79ed0219b9c3ecbd1e'
 default['duo_unix']['configure_options'] = %W{--prefix=/usr}
 default['duo_unix']['use_package'] = true
+
 default['duo_unix']['conf']['integration_key'] = ''
 default['duo_unix']['conf']['secret_key'] = ''
 default['duo_unix']['conf']['api_hostname'] = ''
-default['duo_unix']['conf']['pushinfo'] = 'no'
 default['duo_unix']['conf']['group'] = ''
 default['duo_unix']['conf']['failmode'] = 'safe'
+default['duo_unix']['conf']['pushinfo'] = 'no'
+default['duo_unix']['conf']['http_proxy'] = ''
+default['duo_unix']['conf']['autopush'] = 'no'
+default['duo_unix']['conf']['motd'] = 'no'
+default['duo_unix']['conf']['prompts'] = '3'
+default['duo_unix']['conf']['accept_env_factor'] = 'no'
+default['duo_unix']['conf']['fallback_local_ip'] = 'no'
+
 default['duo_unix']['conf']['PermitTunnel'] = 'no'
 default['duo_unix']['conf']['AllowTCPForwarding'] = 'no'
 default['duo_unix']['conf']['login_duo_enabled'] = true
