c913d233e1d5f084e3fe05a0bda848876204f679
Isaac Elias <nicecurelom@gmail.com>
2016-08-08 11:08:02 -0700

    change to apt older version

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ license          "Apache 2.0"
 description      "Installs/Configures Duo Unix two-factor authentication"
 version          "0.1"
 
-depends			 "apt"
+depends			 "apt", "= 3.0.0"
 depends          "ssh"
